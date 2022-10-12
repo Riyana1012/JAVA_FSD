fc1fe838b9f9a797236fdd6ec9fb0dbc1e737109
Riyana1012 <riyanasaffrin2000@gmail.com>
2022-10-12 21:25:25 +0530

    first commit

---
+++ Phase 5/Project/awsspringboot/src/main/java/com/example/DemoController.java
@@ -0,0 +1,13 @@
+package com.example;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class DemoController {
+@ResponseBody
+@RequestMapping("/display")
+public String display()
+{
+	return "Welcome to deployment of spring boot project...";
+}
+}
